2d90226e04a6b886e9f09e6e1328600134f1a754
Anup Patel <anup.patel@wdc.com>
2018-12-24 10:53:10 +0530

    firmware: Use dummy payload for FW_PAYLOAD
    We use recently added separate dummy payload for FW_PAYLOAD
    when FW_PAYLOAD_PATH is not specified.
    
    Signed-off-by: Anup Patel <anup.patel@wdc.com>

---
+++ firmware/external_deps.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2018 Western Digital Corporation or its affiliates.
+#
+# Authors:
+#   Anup Patel <anup.patel@wdc.com>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+$(build_dir)/$(platform_subdir)/firmware/fw_payload.o: $(FW_PAYLOAD_PATH_FINAL)
