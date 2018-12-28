56e41f7a8db2c9bcfd1116748d496e5d40d2397b
Anup Patel <anup.patel@wdc.com>
2018-12-28 11:01:37 +0530

    platform: Fix author name for Kendryte K210 platform
    The Kendryte K210 platform support was contributed by Damien
    hence fix author name.
    
    Signed-off-by: Anup Patel <anup.patel@wdc.com>

--- platform/kendryte/k210/platform.c
+++ platform/kendryte/k210/platform.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2018 Western Digital Corporation or its affiliates.
  *
  * Authors:
- *   Anup Patel <anup.patel@wdc.com>
+ *   Damien Le Moal <damien.lemoal@wdc.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
