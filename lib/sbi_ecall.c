936bd206c2b1d9f09673bf4c21942f5f127431d9
Alistair Francis <alistair.francis@wdc.com>
2019-02-06 14:02:22 -0800

    sbi_ecall: Fix logical OR to be bitwise OR
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- lib/sbi_ecall.c
+++ lib/sbi_ecall.c
@@ -39,7 +39,7 @@ int sbi_ecall_handler(u32 hartid, ulong mcause,
 	case SBI_ECALL_SET_TIMER:
 #if __riscv_xlen == 32
 		sbi_timer_event_start(scratch,
-			(((u64)regs->a1 << 32) || (u64)regs->a0));
+			(((u64)regs->a1 << 32) | (u64)regs->a0));
 #else
 		sbi_timer_event_start(scratch, (u64)regs->a0);
 #endif
