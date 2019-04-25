c1d01b0c2efea86348235a1c7f83382e53f87e0e
Xiang Wang <wxjstz@126.com>
2019-04-25 11:52:08 +0800

    platform/common/libfdt: fix unnecessary multiple inclusions
    Signed-off-by: Xiang Wang <wxjstz@126.com>

--- platform/common/libfdt/libfdt.h
+++ platform/common/libfdt/libfdt.h
@@ -51,8 +51,8 @@
  *     EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <fdt.h>
 #include <libfdt_env.h>
+#include <fdt.h>
 
 #define FDT_FIRST_SUPPORTED_VERSION	0x02
 #define FDT_LAST_SUPPORTED_VERSION	0x11
