c6d06a94486479bf36f5325d83847d478907195a
Panagiotis Peristerakis <perister@ics.forth.gr>
2019-06-21 11:47:35 +0300

    Changed plic_set_thresh() and plic_set_ie() to public
    Signed-off-by: Panagiotis Peristerakis <perister@ics.forth.gr>

--- include/sbi_utils/irqchip/plic.h
+++ include/sbi_utils/irqchip/plic.h
@@ -18,4 +18,8 @@ int plic_warm_irqchip_init(u32 target_hart, int m_cntx_id, int s_cntx_id);
 
 int plic_cold_irqchip_init(unsigned long base, u32 num_sources, u32 hart_count);
 
+void plic_set_thresh(u32 cntxid, u32 val);
+
+void plic_set_ie(u32 cntxid, u32 word_index, u32 val);
+
 #endif
