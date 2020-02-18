3e7d666d7ca5eca5b4ba9c0c624a1747bbfd7bf7
Bin Meng <bmeng.cn@gmail.com>
2020-02-18 02:18:33 -0800

    platform: qemu: virt: Correct the typo in config.mk
    It should be "aligned".
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Anup Patel <anup.patel@wdc.com>

--- platform/qemu/virt/config.mk
+++ platform/qemu/virt/config.mk
@@ -22,19 +22,19 @@ FW_TEXT_START=0x80000000
 FW_DYNAMIC=y
 FW_JUMP=y
 ifeq ($(PLATFORM_RISCV_XLEN), 32)
-  # This needs to be 4MB alligned for 32-bit system
+  # This needs to be 4MB aligned for 32-bit system
   FW_JUMP_ADDR=0x80400000
 else
-  # This needs to be 2MB alligned for 64-bit system
+  # This needs to be 2MB aligned for 64-bit system
   FW_JUMP_ADDR=0x80200000
 endif
 FW_JUMP_FDT_ADDR=0x82200000
 FW_PAYLOAD=y
 ifeq ($(PLATFORM_RISCV_XLEN), 32)
-  # This needs to be 4MB alligned for 32-bit system
+  # This needs to be 4MB aligned for 32-bit system
   FW_PAYLOAD_OFFSET=0x400000
 else
-  # This needs to be 2MB alligned for 64-bit system
+  # This needs to be 2MB aligned for 64-bit system
   FW_PAYLOAD_OFFSET=0x200000
 endif
 FW_PAYLOAD_FDT_ADDR=0x82200000
