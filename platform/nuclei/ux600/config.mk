4781545512537effc97ffb07079076cf0819842c
Huaqi Fang <578567190@qq.com>
2020-04-28 22:43:40 +0800

    platform: Add Nuclei UX600 platform
    * Nuclei UX600 is a 64-bit RISC-V core developed
      by Nuclei System Technology, see https://nucleisys.com/product.php
    
    * The ISA is configurable in hardware on your demand
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>
    Reviewed-by: Anup Patel <anup.patel@wdc.com>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>

---
+++ platform/nuclei/ux600/config.mk
@@ -0,0 +1,30 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright (c) 2020 Nuclei Corporation or its affiliates.
+#
+# Authors:
+#   lujun <lujun@nucleisys.com>
+#   hqfang <578567190@qq.com>
+#
+
+# Compiler flags
+platform-cppflags-y =
+platform-cflags-y =
+platform-asflags-y =
+platform-ldflags-y =
+
+# Command for platform specific "make run"
+platform-runcmd = xl_spike \
+  $(build_dir)/platform/nuclei/ux600/firmware/fw_payload.elf
+
+# Blobs to build
+FW_TEXT_START=0x80000000
+FW_DYNAMIC=y
+FW_JUMP=y
+
+FW_JUMP_ADDR=0x80200000
+FW_JUMP_FDT_ADDR=0x88000000
+FW_PAYLOAD=y
+FW_PAYLOAD_OFFSET=0x200000
+FW_PAYLOAD_FDT_ADDR=0x88000000
---
+++ platform/nuclei/ux600/objects.mk
@@ -0,0 +1,11 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright (c) 2020 Nuclei Corporation or its affiliates.
+#
+# Authors:
+#   lujun <lujun@nuclesys.com>
+#   hqfang <578567190@qq.com>
+#
+
+platform-objs-y += platform.o
