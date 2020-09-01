dcb10c0056f37d3de6ce2794b8e6f7c322ac4d46
Anup Patel <anup.patel@wdc.com>
2020-09-01 17:50:14 +0530

    lib: sbi: Don't handle VS-mode ecall in sbi_trap_handler()
    The VS-mode ecall is supposed to be handled by HS-mode so
    sbi_trap_handler() should fallback to default case for
    VS-mode ecall trap.
    
    Signed-off-by: Anup Patel <anup.patel@wdc.com>
    Reviewed-by: Atish Patra <atish.patra@wdc.com>

--- lib/sbi/sbi_trap.c
+++ lib/sbi/sbi_trap.c
@@ -251,7 +251,6 @@ void sbi_trap_handler(struct sbi_trap_regs *regs)
 		rc  = sbi_misaligned_store_handler(mtval, mtval2, mtinst, regs);
 		msg = "misaligned store handler failed";
 		break;
-	case CAUSE_SUPERVISOR_ECALL:
 	case CAUSE_HYPERVISOR_ECALL:
 		rc  = sbi_ecall_handler(regs);
 		msg = "ecall handler failed";
