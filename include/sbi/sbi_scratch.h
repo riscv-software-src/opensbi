386eba21bf349201f48e3e01ca129e6bffd7e674
Anup Patel <anup.patel@wdc.com>
2021-01-11 16:45:14 +0530

    include: sbi: No need to pack struct sbi_scratch
    All members in struct sbi_scrach are of machine word size so no need
    to pack this structure. This also allows GCC to generate more optimized
    instruction sequence when accessing members of struct sbi_scratch.
    
    Reported-by: Paul Campbell <taniwha@gmail.com>
    Signed-off-by: Anup Patel <anup.patel@wdc.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Atish Patra <atish.patra@wdc.com>

--- include/sbi/sbi_scratch.h
+++ include/sbi/sbi_scratch.h
@@ -71,7 +71,7 @@ struct sbi_scratch {
 	unsigned long tmp0;
 	/** Options for OpenSBI library */
 	unsigned long options;
-} __packed;
+};
 
 /** Possible options for OpenSBI library */
 enum sbi_scratch_options {
