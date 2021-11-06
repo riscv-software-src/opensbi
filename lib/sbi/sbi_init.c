2fe2f55d5050aac546d35616f2944f889a9c8d6e
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2021-11-06 10:54:49 +0100

    lib: sbi: move sbi_boot_print_general()
    Moving the sbi_boot_print_general() call after the
    sbi_platform_final_init() call allows to print devices initialized in the
    latter.
    
    To keep the overall print sequence the same also move
    sbi_boot_print_domains().
    
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
    Reviewed-by: Anup Patel <anup.patel@wdc.com>

--- lib/sbi/sbi_init.c
+++ lib/sbi/sbi_init.c
@@ -301,8 +301,6 @@ static void __noreturn init_coldboot(struct sbi_scratch *scratch, u32 hartid)
 		sbi_hart_hang();
 	}
 
-	sbi_boot_print_general(scratch);
-
 	/*
 	 * Note: Finalize domains after HSM initialization so that we
 	 * can startup non-root domains.
@@ -315,8 +313,6 @@ static void __noreturn init_coldboot(struct sbi_scratch *scratch, u32 hartid)
 			   __func__, rc);
 		sbi_hart_hang();
 	}
-
-	sbi_boot_print_domains(scratch);
 
 	rc = sbi_hart_pmp_configure(scratch);
 	if (rc) {
@@ -335,6 +331,10 @@ static void __noreturn init_coldboot(struct sbi_scratch *scratch, u32 hartid)
 			   __func__, rc);
 		sbi_hart_hang();
 	}
+
+	sbi_boot_print_general(scratch);
+
+	sbi_boot_print_domains(scratch);
 
 	sbi_boot_print_hart(scratch, hartid);
 
