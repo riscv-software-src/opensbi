d514a8f0dcc3a0a310bbbe33a6ab62d9e10adea8
Anup Patel <apatel@ventanamicro.com>
2022-08-08 09:34:14 +0530

    platform: generic: Use kconfig for enabling/disabling overrides
    We update generic platform to use kconfig for enabling/disabling
    platform overrides. We also enable all platform overrides in generic
    platform defconfig.
    
    Signed-off-by: Anup Patel <apatel@ventanamicro.com>
    Tested-by: Andrew Jones <ajones@ventanamicro.com>
    Acked-by: Atish Patra <atishp@rivosinc.com>
    Tested-by: Atish Patra <atishp@rivosinc.com>

--- platform/generic/allwinner/objects.mk
+++ platform/generic/allwinner/objects.mk
@@ -2,5 +2,5 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 
-carray-platform_override_modules-y += sun20i_d1
-platform-objs-y += allwinner/sun20i-d1.o
+carray-platform_override_modules-$(CONFIG_PLATFORM_ALLWINNER_D1) += sun20i_d1
+platform-objs-$(CONFIG_PLATFORM_ALLWINNER_D1) += allwinner/sun20i-d1.o
