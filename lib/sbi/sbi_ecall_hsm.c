07673fc0633db82db939c0cf896c19ef0a7c548e
Andrew Jones <ajones@ventanamicro.com>
2023-02-27 11:31:00 +0100

    lib: sbi_hsm: Remove unnecessary include
    Also remove a superfluous semicolon and add a blank line.
    
    Signed-off-by: Andrew Jones <ajones@ventanamicro.com>
    Reviewed-by: Anup Patel <anup@brainfault.org>

--- lib/sbi/sbi_ecall_hsm.c
+++ lib/sbi/sbi_ecall_hsm.c
@@ -12,7 +12,6 @@
 #include <sbi/sbi_ecall_interface.h>
 #include <sbi/sbi_error.h>
 #include <sbi/sbi_trap.h>
-#include <sbi/sbi_version.h>
 #include <sbi/sbi_hsm.h>
 #include <sbi/sbi_scratch.h>
 #include <sbi/riscv_asm.h>
@@ -45,7 +44,8 @@ static int sbi_ecall_hsm_handler(unsigned long extid, unsigned long funcid,
 		break;
 	default:
 		ret = SBI_ENOTSUPP;
-	};
+	}
+
 	if (ret >= 0) {
 		*out_val = ret;
 		ret = 0;
