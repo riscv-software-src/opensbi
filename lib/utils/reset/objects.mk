c2e602707dc0f915e96b8726d63d569cbb6724b4
Anup Patel <apatel@ventanamicro.com>
2023-07-21 18:20:27 +0530

    lib: utils/reset: Remove SiFive Test reset driver
    The functionality of SiFive Test reset driver is easily available
    through Syscon reset driver so let us remove the SiFive Test driver.
    
    Signed-off-by: Anup Patel <apatel@ventanamicro.com>

--- lib/utils/reset/objects.mk
+++ lib/utils/reset/objects.mk
@@ -20,9 +20,6 @@ libsbiutils-objs-$(CONFIG_FDT_RESET_GPIO) += reset/fdt_reset_gpio.o
 carray-fdt_reset_drivers-$(CONFIG_FDT_RESET_HTIF) += fdt_reset_htif
 libsbiutils-objs-$(CONFIG_FDT_RESET_HTIF) += reset/fdt_reset_htif.o
 
-carray-fdt_reset_drivers-$(CONFIG_FDT_RESET_SIFIVE_TEST) += fdt_reset_sifive_test
-libsbiutils-objs-$(CONFIG_FDT_RESET_SIFIVE_TEST) += reset/fdt_reset_sifive_test.o
-
 carray-fdt_reset_drivers-$(CONFIG_FDT_RESET_SUNXI_WDT) += fdt_reset_sunxi_wdt
 libsbiutils-objs-$(CONFIG_FDT_RESET_SUNXI_WDT) += reset/fdt_reset_sunxi_wdt.o
 
