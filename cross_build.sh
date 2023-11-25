8a11f3820b2a4cff55b9e9a3b269048e5589f141
InnocentusLime <InnocentusLime.help@gmail.com>
2023-11-25 13:48:41 +0000

    Adjusted the scripts

--- cross_build.sh
+++ cross_build.sh
@@ -2,5 +2,5 @@
 
 export OPENSBI_CC_ABI=lp64d
 export CROSS_COMPILE=riscv64-linux-gnu- 
-make -j4 PLATFORM=generic FW_PAYLOAD=y FW_PAYLOAD_PATH=boot_loader.bin FW_PAYLOAD_OFFSET=0x200000
+make -j4
 
--- make_libsbi.cmd
+++ make_libsbi.cmd
@@ -1,4 +1,3 @@
 set p=%cd%
 
-REM docker run --user %user% -v %p%:/app -it --rm pafos/releng:main /app/cross_build.sh
 docker run -v %p%:/app -it --rm pafos/releng:main /app/cross_build.sh
