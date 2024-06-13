riscv-software-src/opensbi

commit a73ff043e99122be9f50215f04d0a8e93d69a978

lib: utils/reset: Fix fdt_reset to search for more dt nodes
If there are multiple dt nodes, the previous code only tries to match
the first one, which may lose initialization.

Signed-off-by: Xiang W <wxjstz@126.com>
Reviewed-by: Anup Patel <anup@brainfault.org>

lib/utils/reset/fdt_reset.c
@@ -22,22 +22,24 @@ int fdt_reset_driver_init(void *fdt, struct fdt_reset *drv)
 	int noff, rc = SBI_ENODEV;
 	const struct fdt_match *match;
 
-	noff = fdt_find_match(fdt, -1, drv->match_table, &match);
-	if (noff < 0)
-		return SBI_ENODEV;
+	noff = -1;
+	while ((noff = fdt_find_match(fdt, noff,
+				drv->match_table, &match)) >= 0) {
+		if (!fdt_node_is_enabled(fdt, noff))
+			continue;
 
-	if (!fdt_node_is_enabled(fdt, noff))
-		return SBI_ENODEV;
-
-	if (drv->init) {
-		rc = drv->init(fdt, noff, match);
-		if (rc && rc != SBI_ENODEV) {
-			sbi_printf("%s: %s init failed, %d\n",
-				   __func__, match->compatible, rc);
+		if (drv->init) {
+			rc = drv->init(fdt, noff, match);
+			if (rc && rc != SBI_ENODEV) {
+				sbi_printf("%s: %s init failed, %d\n",
+					__func__, match->compatible, rc);
+			}
 		}
+
+		return rc;
 	}
 
-	return rc;
+	return SBI_ENODEV;
 }
 
 void fdt_reset_init(void)

commit 62e178a0a7b989d7cab9b2d4e33b291bc9a58c65

lib: utils/reset: Try initializing all reset devices in dt
In DT, multiple reset devices may use the same driver, and they
may have different priorities. If rc is returned after the first
initialization, the highest priority device may be lost.

Fixes: a73ff043e991 (lib: utils/reset: Fix fdt_reset to search for more dt nodes)
Signed-off-by: Xiang W <wxjstz@126.com>
Reviewed-by: Anup Patel <anup@brainfault.org>

lib/utils/reset/fdt_reset.c
@@ -19,7 +19,7 @@ extern unsigned long fdt_reset_drivers_size;
 
 int fdt_reset_driver_init(void *fdt, struct fdt_reset *drv)
 {
-	int noff, rc = SBI_ENODEV;
+	int noff, rc, cnt = 0;
 	const struct fdt_match *match;
 
 	noff = -1;
@@ -30,16 +30,16 @@ int fdt_reset_driver_init(void *fdt, struct fdt_reset *drv)
 
 		if (drv->init) {
 			rc = drv->init(fdt, noff, match);
-			if (rc && rc != SBI_ENODEV) {
+			if (!rc)
+				cnt++;
+			else if (rc != SBI_ENODEV) {
 				sbi_printf("%s: %s init failed, %d\n",
 					__func__, match->compatible, rc);
 			}
 		}
-
-		return rc;
 	}
 
-	return SBI_ENODEV;
+	return cnt > 0 ? 0 : SBI_ENODEV;
 }
 
 void fdt_reset_init(void)