7e97061d29a6a7b022f4451d1685ea32610e31ee
Gregor Haas <gregorhaas1997@gmail.com>
2024-07-10 13:17:56 -0700

    lib: sbi: Support multiple heaps
    The upcoming SMMTT implementation will require some larger contiguous memory
    regions for the memory tracking tables. We plan to specify the memory region
    for these tables as a reserved-memory node in the device tree, and then
    dynamically allocate individual tables out of this region. These changes to the
    SBI heap allocator will allow us to explicitly create and allocate from a
    dedicated heap tied to the table memory region.

--- include/sbi/sbi_heap.h
+++ include/sbi/sbi_heap.h
@@ -12,6 +12,9 @@
 
 #include <sbi/sbi_types.h>
 
+/* Opaque declaration of heap control struct */
+struct heap_control;
+
 /* Alignment of heap base address and size */
 #define HEAP_BASE_ALIGN			1024
 
@@ -19,9 +22,11 @@ struct sbi_scratch;
 
 /** Allocate from heap area */
 void *sbi_malloc(size_t size);
+void *sbi_malloc_from(struct heap_control *hpctrl, size_t size);
 
 /** Zero allocate from heap area */
 void *sbi_zalloc(size_t size);
+void *sbi_zalloc_from(struct heap_control *hpctrl, size_t size);
 
 /** Allocate array from heap area */
 static inline void *sbi_calloc(size_t nitems, size_t size)
@@ -29,19 +34,32 @@ static inline void *sbi_calloc(size_t nitems, size_t size)
 	return sbi_zalloc(nitems * size);
 }
 
+static inline void *sbi_calloc_from(struct heap_control *hpctrl,
+				    size_t nitems, size_t size)
+{
+	return sbi_zalloc_from(hpctrl, nitems * size);
+}
+
 /** Free-up to heap area */
 void sbi_free(void *ptr);
+void sbi_free_from(struct heap_control *hpctrl, void *ptr);
 
 /** Amount (in bytes) of free space in the heap area */
 unsigned long sbi_heap_free_space(void);
+unsigned long sbi_heap_free_space_from(struct heap_control *hpctrl);
 
 /** Amount (in bytes) of used space in the heap area */
 unsigned long sbi_heap_used_space(void);
+unsigned long sbi_heap_used_space_from(struct heap_control *hpctrl);
 
 /** Amount (in bytes) of reserved space in the heap area */
 unsigned long sbi_heap_reserved_space(void);
+unsigned long sbi_heap_reserved_space_from(struct heap_control *hpctrl);
 
 /** Initialize heap area */
 int sbi_heap_init(struct sbi_scratch *scratch);
+int sbi_heap_init_new(struct heap_control *hpctrl, unsigned long base,
+		       unsigned long size);
+int sbi_heap_alloc_new(struct heap_control **hpctrl);
 
 #endif
