riscv-software-src/opensbi

commit 2dd6eaf680556e763a85b0b4ae180391e2671854

lib: sbi_irqchip: Call driver warm_init from SBI core
Currently, each platform keeps track of which irqchip driver is in use
and calls its warm init function. Since the generic platform may use
multiple irqchip drivers, it has logic to track an array of drivers.

The code is simplified and made common across platforms by treating warm
init and exit as properties of the driver, not the platform. Then the
platform's only role is to select and prepare a driver during cold boot.

For now, only add a .warm_init hook, since none of the existing drivers
need an .exit hook. It could be added in the future if needed.

Signed-off-by: Samuel Holland <samuel.holland@sifive.com>
Reviewed-by: Anup Patel <anup@brainfault.org>

include/sbi_utils/irqchip/plic.h
@@ -42,8 +42,6 @@ void plic_suspend(void);
 
 void plic_resume(void);
 
-int plic_warm_irqchip_init(void);
-
 int plic_cold_irqchip_init(struct plic_data *plic);
 
 #endif

lib/utils/irqchip/plic.c
@@ -190,7 +190,7 @@ void plic_resume(void)
 	plic_delegate(plic);
 }
 
-int plic_warm_irqchip_init(void)
+static int plic_warm_irqchip_init(struct sbi_irqchip_device *dev)
 {
 	struct sbi_scratch *scratch = sbi_scratch_thishart_ptr();
 	const struct plic_data *plic = plic_get_hart_data_ptr(scratch);
@@ -222,6 +222,7 @@ int plic_warm_irqchip_init(void)
 }
 
 static struct sbi_irqchip_device plic_device = {
+	.warm_init	= plic_warm_irqchip_init,
 };
 
 int plic_cold_irqchip_init(struct plic_data *plic)

include/sbi/sbi_irqchip.h
@@ -19,6 +19,9 @@ struct sbi_scratch;
 struct sbi_irqchip_device {
 	/** Node in the list of irqchip devices */
 	struct sbi_dlist node;
+
+	/** Initialize per-hart state for the current hart */
+	int (*warm_init)(struct sbi_irqchip_device *dev);
 };
 
 /**

include/sbi_utils/irqchip/imsic.h
@@ -43,8 +43,6 @@ int imsic_get_target_file(u32 hartindex);
 
 void imsic_local_irqchip_init(void);
 
-int imsic_warm_irqchip_init(void);
-
 int imsic_data_check(struct imsic_data *imsic);
 
 int imsic_cold_irqchip_init(struct imsic_data *imsic);

lib/sbi/sbi_irqchip.c
@@ -40,10 +40,19 @@ int sbi_irqchip_init(struct sbi_scratch *scratch, bool cold_boot)
 {
 	int rc;
 	const struct sbi_platform *plat = sbi_platform_ptr(scratch);
+	struct sbi_irqchip_device *dev;
 
 	rc = sbi_platform_irqchip_init(plat, cold_boot);
 	if (rc)
 		return rc;
+
+	sbi_list_for_each_entry(dev, &irqchip_list, node) {
+		if (!dev->warm_init)
+			continue;
+		rc = dev->warm_init(dev);
+		if (rc)
+			return rc;
+	}
 
 	if (ext_irqfn != default_irqfn)
 		csr_set(CSR_MIE, MIP_MEIP);

lib/utils/irqchip/fdt_irqchip_aplic.c
@@ -16,12 +16,6 @@
 #include <sbi_utils/irqchip/fdt_irqchip.h>
 #include <sbi_utils/irqchip/aplic.h>
 
-static int irqchip_aplic_warm_init(void)
-{
-	/* Nothing to do here. */
-	return 0;
-}
-
 static int irqchip_aplic_cold_init(const void *fdt, int nodeoff,
 				   const struct fdt_match *match)
 {
@@ -55,6 +49,6 @@ static const struct fdt_match irqchip_aplic_match[] = {
 struct fdt_irqchip fdt_irqchip_aplic = {
 	.match_table = irqchip_aplic_match,
 	.cold_init = irqchip_aplic_cold_init,
-	.warm_init = irqchip_aplic_warm_init,
+	.warm_init = NULL,
 	.exit = NULL,
 };

lib/utils/irqchip/fdt_irqchip_imsic.c
@@ -95,5 +95,5 @@ static const struct fdt_match irqchip_imsic_match[] = {
 struct fdt_irqchip fdt_irqchip_imsic = {
 	.match_table = irqchip_imsic_match,
 	.cold_init = irqchip_imsic_cold_init,
-	.warm_init = imsic_warm_irqchip_init,
+	.warm_init = NULL,
 };

lib/utils/irqchip/fdt_irqchip_plic.c
@@ -108,6 +108,6 @@ static const struct fdt_match irqchip_plic_match[] = {
 struct fdt_irqchip fdt_irqchip_plic = {
 	.match_table = irqchip_plic_match,
 	.cold_init = irqchip_plic_cold_init,
-	.warm_init = plic_warm_irqchip_init,
+	.warm_init = NULL,
 	.exit = NULL,
 };

commit 7150db29d7186808cc0df66b54dde1cb0ffe3535

lib: utils: Fix irqchip registration for PLIC and APLIC
Currently, the same irqchip instance is registered for multiple PLIC
and APLIC instances which causes the sbi_list_for_each_entry() loop
in the sbi_irqchip_init() to hang at boot-time.

To address the above issue, register a separate irqchip instance for
each PLIC and APLIC instance.

Fixes: 2dd6eaf68055 ("lib: sbi_irqchip: Call driver warm_init from SBI core")
Reported-by: Himanshu Chauhan <hchauhan@ventanamicro.com>
Signed-off-by: Anup Patel <apatel@ventanamicro.com>
Reviewed-by: Himanshu Chauhan <hchauhan@ventanamicro.com>

include/sbi_utils/irqchip/aplic.h
@@ -12,6 +12,7 @@
 #define __IRQCHIP_APLIC_H__
 
 #include <sbi/sbi_types.h>
+#include <sbi/sbi_irqchip.h>
 
 #define APLIC_MAX_DELEGATE	16
 
@@ -30,6 +31,9 @@ struct aplic_delegate_data {
 };
 
 struct aplic_data {
+	/* Private members */
+	struct sbi_irqchip_device irqchip;
+	/* Public members */
 	unsigned long addr;
 	unsigned long size;
 	unsigned long num_idc;

include/sbi_utils/irqchip/plic.h
@@ -11,8 +11,12 @@
 #define __IRQCHIP_PLIC_H__
 
 #include <sbi/sbi_types.h>
+#include <sbi/sbi_irqchip.h>
 
 struct plic_data {
+	/* Private members */
+	struct sbi_irqchip_device irqchip;
+	/* Public members */
 	unsigned long addr;
 	unsigned long size;
 	unsigned long num_src;

lib/utils/irqchip/aplic.c
@@ -12,7 +12,6 @@
 #include <sbi/sbi_console.h>
 #include <sbi/sbi_domain.h>
 #include <sbi/sbi_error.h>
-#include <sbi/sbi_irqchip.h>
 #include <sbi_utils/irqchip/aplic.h>
 
 #define APLIC_MAX_IDC			(1UL << 14)
@@ -166,9 +165,6 @@ static int aplic_check_msicfg(struct aplic_msicfg_data *msicfg)
 	return 0;
 }
 
-static struct sbi_irqchip_device aplic_device = {
-};
-
 int aplic_cold_irqchip_init(struct aplic_data *aplic)
 {
 	int rc;
@@ -280,7 +276,7 @@ int aplic_cold_irqchip_init(struct aplic_data *aplic)
 	}
 
 	/* Register irqchip device */
-	sbi_irqchip_add_device(&aplic_device);
+	sbi_irqchip_add_device(&aplic->irqchip);
 
 	return 0;
 }

lib/utils/irqchip/plic.c
@@ -15,7 +15,6 @@
 #include <sbi/sbi_domain.h>
 #include <sbi/sbi_error.h>
 #include <sbi/sbi_heap.h>
-#include <sbi/sbi_irqchip.h>
 #include <sbi/sbi_string.h>
 #include <sbi_utils/irqchip/plic.h>
 
@@ -221,10 +220,6 @@ static int plic_warm_irqchip_init(struct sbi_irqchip_device *dev)
 	return 0;
 }
 
-static struct sbi_irqchip_device plic_device = {
-	.warm_init	= plic_warm_irqchip_init,
-};
-
 int plic_cold_irqchip_init(struct plic_data *plic)
 {
 	int i, ret;
@@ -284,7 +279,8 @@ int plic_cold_irqchip_init(struct plic_data *plic)
 	}
 
 	/* Register irqchip device */
-	sbi_irqchip_add_device(&plic_device);
+	plic->irqchip.warm_init = plic_warm_irqchip_init;
+	sbi_irqchip_add_device(&plic->irqchip);
 
 	return 0;
 }