2a6f7ddf87f1534e03e9413fa25394435accacdd
Samuel Holland <samuel.holland@sifive.com>
2025-03-25 16:43:24 -0700

    platform: generic: andes: Remove inline definitions
    The addresses of these functions are used to set function pointers in
    struct platform_override, so it is not valid for them to be inline.
    
    Signed-off-by: Samuel Holland <samuel.holland@sifive.com>
    Reviewed-by: Anup Patel <anup@brainfault.org>
    Link: https://lore.kernel.org/r/20250325234342.711447-2-samuel.holland@sifive.com
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- platform/generic/include/andes/andes_pmu.h
+++ platform/generic/include/andes/andes_pmu.h
@@ -11,24 +11,8 @@
 #include <sbi_utils/fdt/fdt_helper.h>
 #include <sbi_utils/fdt/fdt_pmu.h>
 
-#ifdef CONFIG_ANDES_PMU
-
 int andes_pmu_init(const struct fdt_match *match);
 int andes_pmu_extensions_init(const struct fdt_match *match,
 			      struct sbi_hart_features *hfeatures);
-
-#else
-
-static inline int andes_pmu_init(const struct fdt_match *match)
-{
-	return 0;
-}
-static inline int andes_pmu_extensions_init(const struct fdt_match *match,
-			      struct sbi_hart_features *hfeatures)
-{
-	return 0;
-}
-
-#endif /* CONFIG_ANDES_PMU */
 
 #endif /* _RISCV_ANDES_PMU_H */
