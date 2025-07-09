4825a3f87f8d3ee45b7f9b76ed62de7bd1f2dfa8
Jessica Clarke <jrtc27@jrtc27.com>
2025-07-10 00:28:39 +0100

    include: sbi: Don't use #pragma when preprocessing device tree sources
    Since this persists in the preprocessed output (so that it can affect
    the subsequent compilation), it ends up in the input to dtc and is a
    syntax error, breaking the k210 build. Ideally we wouldn't add the
    -include flag to DTSCPPFLAGS in the first place as this header is wholly
    pointless there, but that's a more invasive build system change compared
    to just making this header safe to include there.
    
    Fixes: 86c01a73ff9d ("lib: sbi: Avoid GOT indirection for global symbol references")
    Signed-off-by: Jessica Clarke <jrtc27@jrtc27.com>
    Reviewed-by: Samuel Holland <samuel.holland@sifive.com>
    Tested-by: Xiang W <wxjstz@126.com>
    Reviewed-by: Xiang W <wxjstz@126.com>
    Link: https://lore.kernel.org/r/20250709232840.37551-1-jrtc27@jrtc27.com
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- include/sbi/sbi_visibility.h
+++ include/sbi/sbi_visibility.h
@@ -7,10 +7,12 @@
 #ifndef __SBI_VISIBILITY_H__
 #define __SBI_VISIBILITY_H__
 
+#ifndef __DTS__
 /*
  * Declare all global objects with hidden visibility so access is PC-relative
  * instead of going through the GOT.
  */
 #pragma GCC visibility push(hidden)
+#endif
 
 #endif
